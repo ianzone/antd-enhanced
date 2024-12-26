2b31caf1c886b7fab402f4ebaa0f4047bd145b1c
ian <yihengz@outlook.com>
2024-12-26 21:46:28 +0800

    feat: 🎸 React 19

--- src/components/display/Calendar/Calender.tsx
+++ src/components/display/Calendar/Calender.tsx
@@ -1,5 +1,7 @@
 /**!SECTION
  * TODO: 实现微软日历
+ * 参考：https://fullcalendar.io/demos
+ * https://github.com/fullcalendar/fullcalendar-react
  * 1. 视图：月视图、周视图、日视图、拆分视图
  * 2. 事件 https://kewisch.github.io/ical.js/api/ICAL.Event.html：创建、编辑、删除、拖拽
  * 3. 多个事件源
